7dafe0e2866d4fa78f705f98dfeaa11ef60c99d0
Glauber Marques Dantas <glauber.md@gmail.com>
2018-02-08 14:34:27 -0200

    Added Grunt tasks to minify the library.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,11 +5,21 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '// <%= pkg.name %> - <%= grunt.template.today("yyyy-mm-dd") %> - Licenced under GPL-3.0\n// Copyright (C) <%= grunt.template.today("yyyy") %> Glauber M. Dantas (opensource@glauber.me)',
+        compress: {
+          sequences: true,
+          dead_code: true,
+          conditionals: true,
+          booleans: true,
+          unused: true,
+          if_return: true,
+          join_vars: true,
+          drop_console: true
+        }
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        src: 'src/js/**/*.js',
+        dest: 'build/cordova-simplewebcall.min.js'
       }
     }
   });
